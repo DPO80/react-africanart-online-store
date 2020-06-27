40f9220cc506497680bad621621a005e4eb1838c
DPO80 <dominicogwuche@gmail.com>
2020-06-27 16:25:51 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,180 @@
+export const storeProducts = [
+{	
+    id: 1,
+    title: "African savanna",
+    img: "img/product-1.png",
+    price: 25,
+    company: "African Art Gallary B.V.",
+    info:
+    "Nick Meeuws 3D African savanna.prevalent within this piece by intricately layering varying sized sheets of card stock, which in turn not only adds character to the bright scene, but also the animals.",	
+    inCart:false,
+    count: 0,
+    total: 0
+},
+{	
+    id: 2,
+    title: "Classical African Art",
+    img: "img/product-2.png",
+    price: 20,
+    company: "African Art Gallary B.V.",
+    info:
+    "This African art print is ideal for your living room and office. Printed on premium photo paper luster. Also available in black and white monochrome print",	
+
+    inCart: false,
+    count: 0,
+    total: 0
+},
+
+{	
+    id: 3,
+    title: "Sunset African Village Scene",
+    img: "img/product-3.png",
+    price: 20,
+    company: "African Art Gallary B.V.",
+    info:
+    "This African art print is ideal for your living room and office. Printed on premium photo paper luster. Also available in black and white monochrome print",	
+
+    inCart: false,
+    count: 0,
+    total: 0
+},
+
+{	
+    id: 4,
+    title: "Sunset Painting",
+    img: "img/product-4.png",
+    price: 17,
+    company: "African Art Gallary B.V.",
+    info:
+    "Sunset wildlife painting with a beatiful African landscape. Colors are bright, full of layers, glow as the focal point of a room. It looks peaceful. It calms your mind, a warm touch as a home decor for busy life.",	
+
+    inCart: false,
+    count: 0,
+    total: 0
+},
+
+{	
+    id: 5,
+    title: "Nomadic Art Scene",
+    img: "img/product-5.png",
+    price: 15,
+    company: "African Art Gallary B.V.",
+    info:
+    "This art work shows a typical normadic Fulani culture of Northern Nigeria. This figure shows a man standing in a relaxed position, balancing a cane on his shoulders with his arms looped over it.  The Fulani use their canes to direct cattle when they are moving across land, as well as to support themselves when they are tired!",	
+
+    inCart: false,
+    count: 0,
+    total: 0
+},
+
+{	
+    id: 6,
+    title: "Nomadic Art Scene",
+    img: "img/product-6.png",
+    price: 15,
+    company: "African Art Gallary B.V.",
+    info:
+    "This art work shows a typical normadic Fulani village setting in Northern Nigeria.",	
+
+    inCart: false,
+    count: 0,
+    total: 0
+},
+
+{	
+    id: 7,
+    title: "African Theme Village Setting",
+    img: "img/product-7.png",
+    price: 20,
+    company: "African Art Gallary B.V.",
+    info:
+    "A selection of African hand painted pictures depicting an African Village.A typical African theme village settings in a variety of colors. The back of the paintings have African vegetation.",	
+
+    inCart: false,
+    count: 0,
+    total: 0
+},
+
+{	
+    id: 8,
+    title: "Decor Abstract Painting",
+    img: "img/product-8.png",
+    price: 20,
+    company: "African Art Gallary B.V.",
+    info:
+    "This African art print is ideal for your living room and office. Printed on premium photo paper luster. Also available in black and white monochrome print.",	
+
+    inCart: false,
+    count: 0,
+    total: 0
+},
+
+{	
+    id: 9,
+    title: "Food Prep-Painting",
+    img: "img/product-9.png",
+    price: 12.50,
+    company: "African Art Gallary B.V.",
+    info:
+    "Print on paper, village scene of an African woman prepearing meal. This painting is made from archival matte paper engineered for accurate color reproduction that provides high contrast and high resolution output. This paper is acid-free making it the perfect choice for both photography & fine art reproductions.",	
+
+    inCart: false,
+    count: 0,
+    total: 0
+},
+
+{	
+    id: 10,
+    title: "Map of African",
+    img: "img/product-10.png",
+    price: 10,
+    company: "African Art Gallary B.V.",
+    info:
+    "An elegant dark brown map of Africa. This wood work looks gorgeous on your wall, making it the perfect match for both modern and classic homes.Thanks to its stately brown design, a simply way to revamp your entire African experience.",	
+    inCart: false,
+    count: 0,
+    total: 0
+},
+
+{	
+    id: 11,
+    title: "Traditional African Women Canvas",
+    img: "img/product-11.png",
+    price: 40,
+    company: "African Art Gallary B.V.",
+    info:
+    "Village scene of an African women. This painting is made from archival matte paper engineered for accurate color reproduction that provides high contrast and high resolution output. This paper is acid-free making it the perfect choice for both photography & fine art reproductions.",	
+
+    inCart: false,
+    count: 0,
+    total: 0
+},
+
+{	
+    id: 12,
+    title: "Banana Leaf Art work",
+    img: "img/product-12.png",
+    price: 50,
+    company: "African Art Gallary B.V.",
+    info:
+    "This art work is ideal for your living room and office.",	
+
+    inCart: false,
+    count: 0,
+    total: 0
+},
+
+];
+
+export const detailProduct = {
+    id: 1,
+    title: "African savanna",
+    img: "img/product-1.png",
+    price: 50,
+    company: "African Art Gallary B.V.",
+    info:
+    "Nick Meeuws 3D African savanna. Prevalent within this piece by intricately layering varying sized sheets of card stock, which in turn not only adds character to the bright scene, but also the animals.",	
+    inCart: false,
+    count: 0,
+    total: 0
+};
